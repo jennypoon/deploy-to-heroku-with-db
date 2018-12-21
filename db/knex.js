6aab60cd7b95494f117f534e05a7335753bb2068
Jenny Poon <jenny.poon@live.ca>
2018-12-20 16:51:11 -0800

    added console.logs for troubleshooting

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 var config = require('../knexfile')[env];
 
+console.log("ENV", env)
+console.log("CONFIG", config)
+
 module.exports = require('knex')(config);
